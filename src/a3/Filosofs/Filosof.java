a22d1daf71a0503eade9db7c57d3b57c816a7881
danilocick <56125505+danilocick@users.noreply.github.com>
2021-02-19 17:20:39 +0100

    a3 working

--- src/a3/Filosofs/Filosof.java
+++ src/a3/Filosofs/Filosof.java
@@ -17,7 +17,7 @@ public class Filosof extends Thread {
             //Agafa el comandament
             cobert1.Agafa();
             cobert2.Agafa();
-            System.out.println(nom+": tinc els coberts: "+cobert1.getNom()+" i "+cobert2.getNom());
+            System.out.println(getNom()+": tinc els coberts: "+cobert1.getNom()+" i "+cobert2.getNom());
 
             //Menja
             try {
@@ -30,11 +30,15 @@ public class Filosof extends Thread {
             cobert2.Deixa();
             //Descansa de tanta tele
             try {
-                System.out.println("Ja he acabat");
+                System.out.println(getNom()+": ja he acabat");
                 Thread.sleep((long) (Math.random()*1000)+500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public synchronized String getNom() {
+        return nom;
     }
 }
